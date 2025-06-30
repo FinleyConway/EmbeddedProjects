08b1ce19b07ae334e84f20fd9fa578edd1373134
finleyconway <finleyconway03@gmail.com>
2025-06-30 13:19:58 +0100

    Scrolling Text

--- dot_matrix/dot_matrix.ino
+++ dot_matrix/dot_matrix.ino
@@ -2,13 +2,12 @@
 
 LedControl matrix = LedControl(12, 10, 11, 1);
 
-#define WIDTH 8
-#define HEIGHT 8
+#define CHAR_WIDTH 7
+#define CHAR_HEIGHT 5
+#define CHAR_SPACING 1
 
-struct moving_char // 10 bytes
+struct moving_char // 8 bytes
 {
-  uint8_t x;
-  uint8_t y;
   uint8_t letter_rows[8];
 };
 
@@ -25,7 +24,6 @@ Due to bitset limitations, could have a bitset buffer and push and move these bi
   - 
 */
 moving_char char_a = {
-  0, 0,
   {
     0b01110000,
     0b10001000,
@@ -39,7 +37,6 @@ moving_char char_a = {
 };
 
 moving_char char_b = {
-  0, 0,
   {
     0b01111000,
     0b10001000,
@@ -62,21 +59,53 @@ void setup()
   matrix.clearDisplay(0);
 
   Serial.begin(9600);
-
-  memcpy(bitset_buffer, char_a.letter_rows, 8);
 }
+
+moving_char moving_word[3] = {
+  char_b,
+  char_a,
+  char_b
+}; // bab
+int current_char = 0;
+int current_width = 0;
 
 void loop()
 {
   delay(250);
 
+  const moving_char& letter = moving_word[current_char];
+
+  // write to buffer
   for (int i = 0; i < 8; i++)
   {
-    uint8_t value = bitset_buffer[i];
+    // get the current bit from current row
+    uint8_t bitset = letter.letter_rows[i];
+    uint8_t bit = bitRead(bitset, current_width);
 
-    bitset_buffer[i] = (value >> 1) | (value << (8 - 1));
+    // assign that bit the last bit in the buffer bitset
+    bitset_buffer[i] = bitWrite(bitset_buffer[i], 7, bit);
   }
 
+  current_width++;
+
+  if (current_width > CHAR_WIDTH)
+  {
+    current_width = 0;
+    current_char++;
+
+    if (current_char > 3)
+    {
+      current_char = 0;
+    }
+  }
+
+  // moves characters
+  for (int i = 0; i < 8; i++)
+  {
+    bitset_buffer[i] >>= 1;
+  }
+
+  // draws dots
   for (int i = 0; i < 8; i++)
   {
     matrix.setColumn(0, i, bitset_buffer[i]);
