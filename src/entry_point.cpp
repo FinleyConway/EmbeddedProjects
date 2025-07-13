2f367ea4030f84d681014c0b4e23b63eab143ad1
finleyconway <finleyconway03@gmail.com>
2025-07-13 18:51:53 +0000

    Dino game exploiter code

--- src/entry_point.cpp
+++ src/entry_point.cpp
@@ -1,8 +1,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#include <Arduino.h>
-
 void set_pin(volatile uint8_t* port, uint8_t pin)
 {
     *port |= (1 << pin);
@@ -17,6 +15,28 @@ bool read_pin(volatile uint8_t* port, uint8_t pin)
 {
     return *port & (1 << pin);
 }
+
+// https://github.com/adgzlanl/atmega2560-Reading-ADC-example/blob/master/Serial_ADC.c
+void init_adc()
+{
+    // Select Vref=AVcc
+    ADMUX |= (1<<REFS0);
+    //set prescaller to 128 and enable ADC 
+    ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);    
+}
+
+uint16_t read_adc(uint8_t ADCchannel)
+{
+    //select ADC channel with safety mask
+    ADMUX = (ADMUX & 0xF0) | (ADCchannel & 0x0F);
+    //single conversion mode
+    ADCSRA |= (1<<ADSC);
+    // wait until ADC conversion is complete
+    while( ADCSRA & (1<<ADSC) );
+
+    return ADC;
+}
+
 
 void delay_micro(int ms)
 {
@@ -52,7 +72,7 @@ void move_pulse(uint8_t degrees, const uint8_t pulse_cycle = 20)
     uint32_t pulse_width = ((uint32_t)(degrees - min_degrees) * (high_range - low_range)) /
                             (max_degrees - min_degrees) + low_range;
 
-    // send 50 pwm pulses
+    // send 51 pwm pulses
     for (uint8_t pc = 0; pc <= 50; pc++)
     {
         set_pin(&PORTB, PORTB7);
@@ -68,21 +88,19 @@ int main()
     set_pin(&DDRB, DDB7); // pin 13
     set_pin(&DDRH, DDB4); // pin 7
 
-    // input pullup
-    unset_pin(&DDRB, DDB6);
-    set_pin(&PORTB, PB6);
+    init_adc();
 
     move_pulse(0);
 
     while (true)
     {
-        bool pin_state = read_pin(&PINB, PB6);
+        uint16_t value = read_adc(0);
 
-        if (pin_state)
+        if (value <= 695)
         {
             set_pin(&PORTH, PORTH4);
 
-            move_pulse(0, 0);
+            move_pulse(70, 0);
         }
         else
         {
